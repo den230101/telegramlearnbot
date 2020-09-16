393dd1fcc957ceacb50630f4d5632d9fb4eabf1e
den230101 <den230101@example.com>
2020-09-16 11:16:55 +0300

    With emoji

--- bot.py
+++ bot.py
@@ -2,6 +2,7 @@ from glob import glob
 import logging
 from random import choice
 
+from emoji import emojize
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
 import settings
@@ -16,25 +17,34 @@ def main():
     mybot = Updater(settings.API_KEY)
     logging.info('Бот запускается')
     dp = mybot.dispatcher
-    dp.add_handler(CommandHandler("start", greet_user))
-    dp.add_handler(CommandHandler("cat", send_cat_picture))
-    dp.add_handler(MessageHandler(Filters.text, talk_to_me))
+    dp.add_handler(CommandHandler("start", greet_user, pass_user_data=True))
+    dp.add_handler(CommandHandler("cat", send_cat_picture, pass_user_data=True))
+    dp.add_handler(MessageHandler(Filters.text, talk_to_me, pass_user_data=True))
     mybot.start_polling()
     mybot.idle()
 
-def talk_to_me(bot, update):
-    user_text = "Привет {}! Ты написал: '{}'".format(update.message.chat.first_name, update.message.text)
+def talk_to_me(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    user_text = "Привет {} {}! Ты написал: '{}'".format(update.message.chat.first_name, emo, update.message.text)
     logging.info("User: {}, Chat_id: {}, Message: {}".format(update.message.chat.first_name, update.message.chat.id, update.message.text))
     update.message.reply_text(user_text)
 
-def greet_user(bot, update):
-    text = 'Вызван /start'
+def greet_user(bot, update, user_data):
+    emo = get_user_emo(user_data)
+    text = 'Привет {}'.format(emo)
     logging.info(text)
     update.message.reply_text(text)
 
-def send_cat_picture(bot, update):
+def send_cat_picture(bot, update, user_data):
     cat_list = glob('images/*.jpg')
     cat_pic = choice(cat_list)
     bot.send_photo(chat_id=update.message.chat.id, photo=open(cat_pic, 'rb'))
+
+def get_user_emo(user_data):
+    if 'emo' in user_data:
+        pass
+    else:
+        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
+    return user_data['emo']
 
 main()
